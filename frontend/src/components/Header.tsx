14976efcd615a7edd5b1d9bbe3f3c6ac44c66aba
Joseph Mutua <Mutuaj793@gmail.com>
2024-06-10 09:09:21 +0300

    Minor fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import { Box, Typography } from "@mui/material";
-// import { useTheme } from "@mui/material/styles";
-
-// const Header: React.FC = () => {
-//   const theme = useTheme();
-//   return (
-//     <Box
-//       sx={{
-//         display: "flex",
-//         alignItems: "center",
-//         justifyContent: "center",
-//         marginBottom: 4,
-//       }}
-//     >
-//       <img
-//         src={new URL(`../../public/Logo.svg`, import.meta.url).href}
-//         alt="App Icon"
-//         style={{ width: 100, height: 100, marginRight: 16 }}
-//       />
-//       <Typography
-//         variant="h3"
-//         sx={{ color: theme.palette.text.primary, margin: "20px 0" }}
-//       >
-//         Book Assignment
-//       </Typography>
-//     </Box>
-//   );
-// };
-
-// export default Header;
 
 
 import React from "react";
